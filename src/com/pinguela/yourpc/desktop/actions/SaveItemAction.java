1ceb48626b366e571a28d6237d9826f576e473a7
Alexandre <Alexandre@DESKTOP-P4C2FN3>
2024-05-21 15:23:33 +0200

    Factorized dialog actions

---
+++ src/com/pinguela/yourpc/desktop/actions/SaveItemAction.java
@@ -0,0 +1,57 @@
+package com.pinguela.yourpc.desktop.actions;
+
+import java.awt.Component;
+
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.pinguela.YPCException;
+import com.pinguela.yourpc.desktop.view.ItemView;
+
+public abstract class SaveItemAction<T> 
+extends ItemAction<T> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1974717185749303944L;
+
+	private static Logger logger = LogManager.getLogger(SaveItemAction.class);
+
+	public SaveItemAction(ItemView<T> view, Icon icon) {
+		super(view, icon);
+	}
+
+	public SaveItemAction(ItemView<T> view, String name, Icon icon) {
+		super(view, name, icon);
+	}
+
+	public SaveItemAction(ItemView<T> view, String name) {
+		super(view, name);
+	}
+
+	public SaveItemAction(ItemView<T> view) {
+		super(view);
+	}
+
+	@Override
+	protected void doAction() {
+		try {
+			doSave();
+			if (!getView().showCard(ItemView.ACTION_CARD)) {
+				getView().addAction(new ItemEditAction<T>(getView()), ItemView.ACTION_CARD);
+				getView().showCard(ItemView.ACTION_CARD);
+			} 
+		} catch (YPCException e) {
+			logger.error(e.getMessage(), e);
+			JOptionPane.showMessageDialog((Component) getView(), 
+					"An error occured while updating the product.", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	protected abstract void doSave() throws YPCException;
+
+}

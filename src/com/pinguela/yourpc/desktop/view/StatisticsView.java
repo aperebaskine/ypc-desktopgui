3a2129a1cef7489f733c7450de3a5dddeb693ef9
Alexandre <pereb27@gmail.com>
2024-06-17 18:27:51 +0200

    Fix indentation

--- src/com/pinguela/yourpc/desktop/view/StatisticsView.java
+++ src/com/pinguela/yourpc/desktop/view/StatisticsView.java
@@ -24,20 +24,22 @@ public class StatisticsView extends YPCView {
 	 * 
 	 */
 	private static final long serialVersionUID = 235760785135773930L;
-	
+
+
+
 	private JPanel criteriaPanel;
 	private JPanel chartPanel;
-	
+
 	private JFormattedTextField productIdField;
 	private JDateChooser startDateChooser;
 	private JDateChooser endDateChooser;
 	private JButton searchButton;
-	
+
 	public StatisticsView() {
 		initialize();
 		postInitialize();
 	}
-	
+
 	private void initialize() {
 		criteriaPanel = new JPanel();
 		add(criteriaPanel, BorderLayout.NORTH);
@@ -47,14 +49,14 @@ public class StatisticsView extends YPCView {
 		gbl_criteriaPanel.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		gbl_criteriaPanel.rowWeights = new double[]{1.0, 1.0, 0.0, Double.MIN_VALUE};
 		criteriaPanel.setLayout(gbl_criteriaPanel);
-		
+
 		JLabel dateFromLabel = new JLabel("From:");
 		GridBagConstraints gbc_dateFromLabel = new GridBagConstraints();
 		gbc_dateFromLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_dateFromLabel.gridx = 0;
 		gbc_dateFromLabel.gridy = 0;
 		criteriaPanel.add(dateFromLabel, gbc_dateFromLabel);
-		
+
 		JLabel productIdLabel = new JLabel("Product ID:");
 		GridBagConstraints gbc_productIdLabel = new GridBagConstraints();
 		gbc_productIdLabel.anchor = GridBagConstraints.EAST;
@@ -62,7 +64,7 @@ public class StatisticsView extends YPCView {
 		gbc_productIdLabel.gridx = 3;
 		gbc_productIdLabel.gridy = 0;
 		criteriaPanel.add(productIdLabel, gbc_productIdLabel);
-		
+
 		productIdField = new JFormattedTextField();
 		GridBagConstraints gbc_formattedTextField = new GridBagConstraints();
 		gbc_formattedTextField.insets = new Insets(0, 0, 5, 0);
@@ -70,7 +72,7 @@ public class StatisticsView extends YPCView {
 		gbc_formattedTextField.gridx = 4;
 		gbc_formattedTextField.gridy = 0;
 		criteriaPanel.add(productIdField, gbc_formattedTextField);
-		
+
 		JLabel dateToLabel = new JLabel("To:");
 		GridBagConstraints gbc_dateToLabel = new GridBagConstraints();
 		gbc_dateToLabel.insets = new Insets(0, 0, 5, 5);
@@ -78,9 +80,9 @@ public class StatisticsView extends YPCView {
 		gbc_dateToLabel.gridy = 1;
 		criteriaPanel.add(dateToLabel, gbc_dateToLabel);
 	}
-	
+
 	private void postInitialize() {
-		
+
 		Calendar c = Calendar.getInstance();
 		c.add(Calendar.MONTH, -1);
 		startDateChooser = ComponentFactory.getDateChooser(c);
@@ -90,8 +92,8 @@ public class StatisticsView extends YPCView {
 		gbc_startDateChooser.gridx = 1;
 		gbc_startDateChooser.gridy = 0;
 		criteriaPanel.add(startDateChooser, gbc_startDateChooser);
-		
-		
+
+
 		endDateChooser = ComponentFactory.getDateChooser(Calendar.getInstance());
 		GridBagConstraints gbc_endDateChooser = new GridBagConstraints();
 		gbc_endDateChooser.insets = new Insets(0, 0, 5, 5);
@@ -99,7 +101,7 @@ public class StatisticsView extends YPCView {
 		gbc_endDateChooser.gridx = 1;
 		gbc_endDateChooser.gridy = 1;
 		criteriaPanel.add(endDateChooser, gbc_endDateChooser);
-		
+
 		searchButton = new JButton(new RetrieveStatisticsAction(this));
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.anchor = GridBagConstraints.EAST;
@@ -107,19 +109,19 @@ public class StatisticsView extends YPCView {
 		gbc_btnNewButton.gridy = 2;
 		criteriaPanel.add(searchButton, gbc_btnNewButton);
 	}
-	
+
 	// TODO : Create StatisticsCriteria object
 	public ProductCriteria getCriteria() {
 		ProductCriteria criteria = new ProductCriteria();
-		
+
 		criteria.setId(Long.valueOf(productIdField.getText()));
 		criteria.setLaunchDateMin(startDateChooser.getDate());
 		criteria.setLaunchDateMax(endDateChooser.getDate());
-		
+
 		return criteria;
-		
+
 	}
-	
+
 	public void showChart(JFreeChart chart) {
 		if (chartPanel != null) {
 			remove(chartPanel);
